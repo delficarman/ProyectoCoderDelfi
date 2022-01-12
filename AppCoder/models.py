b6a142cd2153a914c8114826b34b39fb34a96d6b
Delfina Carman <delficarman@hotmail.com>
2022-01-12 16:44:45 -0300

    second commit

--- AppCoder/models.py
+++ AppCoder/models.py
@@ -4,11 +4,26 @@ from django.db import models
 
 class Curso(models.Model):
 
-    nombre = models.CharField('nombre',max_length=40)
+    nombre = models.CharField(max_length=40)
     camada = models.IntegerField()
 
-class Alumno(models.Model):
+class Estudiante(models.Model):
 
-    nombre = models.CharField('nombre',max_length=50)
-    apellido = models.CharField('apellido',max_length=50)
-    fecha_inscripcion = models.DateField('fecha',auto_now=False,auto_now_add=False)
+    nombre = models.CharField(max_length=30)
+    apellido = models.CharField(max_length=30)
+    email = models.EmailField()
+
+class Profesor(models.Model):
+
+    nombre = models.CharField(max_length=30)
+    apellido = models.CharField(max_length=30)
+    email = models.EmailField()
+    profesion = models.CharField(max_length=30)
+
+class Entregable(models.Model):
+
+    nombre = models.CharField(max_length=30)
+    fechaDeEntrega = models.DateField()
+    entregado = models.BooleanField()
+
+
